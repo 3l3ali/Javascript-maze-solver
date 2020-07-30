29c9ee83106ef5e9880bc1040464a68856fa8223
Moe Alali <moe3l3ali@gmail.com>
2020-07-30 03:56:12 +0300

    added wow js animations

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,18 +1,23 @@
 import React from "react";
-import '../css/home.css';
 import { Link } from 'react-router-dom';
+import {WOW} from 'wowjs';
+import '../css/home.css';
+import '../../node_modules/wowjs/css/libs/animate.css'
 
 class Home extends React.Component {
+    componentDidMount() {
+        new WOW().init();
+    }
 
     render() {
         return(
             <div id="banner" className="pickgradient">
                 <div className="container">
                     <div className="row text-center">
-                        <div className="col-12">
+                        <div className="col-12 wow fadeInUp" data-wow-delay="0.3s">
                             <h1>The Maze Solver</h1>
                         </div>
-                        <div className="col-12 text-center">
+                        <div className="col-12 text-center animated rollIn infinite">
                             <Link to={'/maze'}>
                                 <div id="start"></div>
                             </Link> 
